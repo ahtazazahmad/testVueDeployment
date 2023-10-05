f29e7e978474827914512b1ab6588fd400e7fdf3
Ahtazaz Ahmad <ahtazazahmad.appelit@nextbridge.com>
2023-10-05 15:11:35 +0500

    first

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <NavigationCompnent></NavigationCompnent>
+  <router-view />
+</template>
+<script>
+import NavigationCompnent from "./components/navigation/NavigationComponent.vue";
+export default {
+  name: "AppRoot",
+  props: {},
+  components: {
+    NavigationCompnent,
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-root">
+    <HomeComponent />
+  </div>
+</template>
+
+<script>
+import HomeComponent from "@/components/home/Index.vue";
+import { mapActions, mapMutations } from "vuex";
+export default {
+  name: "HomeView",
+  components: {
+    HomeComponent,
+  },
+  mounted() {
+    this.requestAPIForGameList().then(() => {});
+  },
+  methods: {
+    ...mapMutations(["mutateDataLoadingStatus"]),
+    ...mapActions(["requestAPIForGameList"]),
+  },
+};
+</script>
